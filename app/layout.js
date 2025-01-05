f06a03efa0c5474b41ce90acf0dfea83594a6440
anirudhj2000 <anirudhjoshi985@gmail.com>
2025-01-05 15:09:19 +0530

    modal changes

--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,25 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { Inter } from "next/font/google";
-import "./globals.css";
+'use client';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
   const router = useRouter();
 
-  useEffect(() => {
-    console.log("RootLayout");
-    const token = localStorage.getItem("token");
+  // useEffect(() => {
+  //   console.log("RootLayout");
+  //   const token = localStorage.getItem("token");
 
-    if (!token) {
-      router.push("/login");
-    }
-  }, []);
+  //   if (!token) {
+  //     router.push("/login");
+  //   }
+  // }, []);
 
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>{children}</body>
     </html>
   );
